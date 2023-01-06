b30896197bf7e56c70ea00e038c13f3f7b8eb9e1
Jan Philipp Berg <git.7ksst@aleeas.com>
2023-01-06 18:54:20 +0100

    Add documentation to the class 'Runner'

--- Spell Checker/src/main/Runner.java
+++ Spell Checker/src/main/Runner.java
@@ -1,7 +1,18 @@
 package main;
 
+/**
+ * Runner for the TypoTattler. Entry point for the program.
+ * @author Jan Philipp Berg
+ * @vers 0.1
+ *
+ */
 public class Runner {
 
+	/**
+	 * Main method for the TypoTattler. Responsible for creating a new instance and
+	 * launching the mainloop.
+	 * @param args command line arguments
+	 */
 	public static void main(String[] args) {
 		TypoTattler tt = new TypoTattler(args);
 		tt.mainloop();
