8873695bc33a1029277f3e37b2be777761ef0c21
Jan Philipp Berg <git.7ksst@aleeas.com>
2023-02-01 19:59:43 +0100

    Add licensing
    TypoTattler is now licensed under the GNU GPLv3 license.

--- Spell Checker/src/main/TypoTattler.java
+++ Spell Checker/src/main/TypoTattler.java
@@ -1,4 +1,20 @@
-/* LICENSING MISSING */
+/**
+ * Copyright (C) 2023 Jan Philipp Berg <git.7ksst@aleeas.com>
+ * 
+ * This file is part of TypoTattler.
+ * 
+ * TypoTattler is free software: you can redistribute it and/or modify it under the terms of
+ * the GNU General Public License as published by the Free Software Foundation, either 
+ * version 3 of the License, or (at your option) any later version.
+ * 
+ * TypoTattler is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with TypoTattler. 
+ * If not, see <https://www.gnu.org/licenses/>. 
+ */
+
 package main;
 import java.io.FileNotFoundException;
 import java.io.IOException;
